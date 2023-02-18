3e08e0aae652eb8969f0e81fe471fbe50b2fe163
KCmean <kunalmanu2468@gmail.com>
2023-02-19 04:39:28 +0530

    changes ... good changes

--- dashboard/src/pages/BlogPage.js
+++ dashboard/src/pages/BlogPage.js
@@ -1,24 +1,24 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { Grid, Button, Container, Stack, Typography, TabPanel } from '@mui/material';
+import { Grid, Button, Container, Stack, Typography, TabPanel, Box } from '@mui/material';
 // components
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import { map } from "lodash";
 
 export default function BlogPage() {
-    const mapContainerRef = React.useRef(null);
+    // const mapContainerRef = React.useRef(null);
   
-    React.useEffect(() => {
-      mapboxgl.accessToken = 'pk.eyJ1IjoiZXNoYW50cml2ZWRpMjEiLCJhIjoiY2xjaXV6c2lqMTFzNjNvcXVmbzM0aGkwNyJ9.ZsRWT2z--97ajM58KQG4xQ';
-      const map = new mapboxgl.Map({
-        container: mapContainerRef.current,
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [-122.4194, 37.7749],
-        zoom: 1
-      });
-    }, []);
+    // React.useEffect(() => {
+    //   mapboxgl.accessToken = 'pk.eyJ1IjoiZXNoYW50cml2ZWRpMjEiLCJhIjoiY2xjaXV6c2lqMTFzNjNvcXVmbzM0aGkwNyJ9.ZsRWT2z--97ajM58KQG4xQ';
+    //   const map = new mapboxgl.Map({
+    //     container: mapContainerRef.current,
+    //     style: 'mapbox://styles/mapbox/streets-v12',
+    //     center: [-122.4194, 37.7749],
+    //     zoom: 1
+    //   });
+    // }, []);
   return (
     <>
       <Container>
@@ -28,8 +28,16 @@ export default function BlogPage() {
       <Typography variant="subtitle2" >
             loading map embedded data...
       </Typography>
-      <div ref={mapContainerRef} className="map-container" style={{ width:'400', height:'400'}} />
       </Container>
+      <Box
+      sx={{
+        // width: '100% !important',
+        // height: '100% !important',
+        objectFit: 'cover',
+      }}
+      >
+        <img className="w-full h-auto mt-5"src="https://img.freepik.com/premium-vector/city-map-background-blue-tone_99087-101.jpg?w=360"/>
+      </Box>
 
     </>
   )
