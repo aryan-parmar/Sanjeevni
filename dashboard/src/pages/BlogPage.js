da37cb393ffedf7d7a8c53ca3f6386aa895afe3e
KCmean <kunalmanu2468@gmail.com>
2023-02-19 04:00:11 +0530

    mapbox : added map feature in blog

--- dashboard/src/pages/BlogPage.js
+++ dashboard/src/pages/BlogPage.js
@@ -1,17 +1,24 @@
+import React, { useRef, useEffect, useState } from "react";
 import { Helmet } from 'react-helmet-async';
-import {useref} from 'react';
 // @mui
 import { Grid, Button, Container, Stack, Typography, TabPanel } from '@mui/material';
 // components
-// import "mapbox-gl/dist/mapbox-gl.css";
-// import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-// import ReactMapGL, { Marker, Popup } from "react-map-gl";
-
-// mapboxgl.accessToken =
-//   "pk.eyJ1IjoiZXNoYW50cml2ZWRpMjEiLCJhIjoiY2xjaXV6c2lqMTFzNjNvcXVmbzM0aGkwNyJ9.ZsRWT2z--97ajM58KQG4xQ";
-
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { map } from "lodash";
 
 export default function BlogPage() {
+    const mapContainerRef = React.useRef(null);
+  
+    React.useEffect(() => {
+      mapboxgl.accessToken = 'pk.eyJ1IjoiZXNoYW50cml2ZWRpMjEiLCJhIjoiY2xjaXV6c2lqMTFzNjNvcXVmbzM0aGkwNyJ9.ZsRWT2z--97ajM58KQG4xQ';
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-122.4194, 37.7749],
+        zoom: 1
+      });
+    }, []);
   return (
     <>
       <Container>
@@ -21,8 +28,9 @@ export default function BlogPage() {
       <Typography variant="subtitle2" >
             loading map embedded data...
       </Typography>
-
+      <div ref={mapContainerRef} className="map-container" style={{ width:'400', height:'400'}} />
       </Container>
+
     </>
-  );
+  )
 }
