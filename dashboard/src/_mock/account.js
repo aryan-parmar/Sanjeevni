5b58884342efa19a81eaaee4babf0e895967aee7
KCmean <kunalmanu2468@gmail.com>
2023-02-18 10:46:35 +0530

    dashboard : 1.0.0

---
+++ dashboard/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Gov. Of India',
+  email: 'demo@minimals.cc',
+  photoURL: '/assets/images/avatars/avatar_default_2.jpg',
+};
+
+export default account;
